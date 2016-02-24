cdb324859677efd8b91a5ddcdd21e1e3d0059d61
Derek Pressnall <derekpressnall@yahoo.com>
2016-02-24 11:23:24 -0600

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,1 +1,13 @@
 # This controller is for all the CRUD operations related to a User.
+MyApp.get "/new_user" do 
+  erb :"users/new_user"
+end
+
+MyApp.post "/new_user/create" do
+  @user = User.new
+  @user.name = params["name"]
+  @user.email = params["email"]
+  @user.password = params["password"]
+  @user.save
+  erb :"users/user_success"
+end
