39e5037d510aa36a1bc761c20726d21db2eb4008
Derek Pressnall <derekpressnall@yahoo.com>
2016-02-24 11:59:43 -0600

    nothing appearing in sinatra

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 # This controller is for all the CRUD operations related to a User.
-MyApp.get "/new_user" do 
+#new user form
+MyApp.get "/" do 
   erb :"users/new_user"
 end
 
+#creates new user
 MyApp.post "/new_user/create" do
   @user = User.new
   @user.name = params["name"]
@@ -12,7 +14,26 @@ MyApp.post "/new_user/create" do
   erb :"users/user_success"
 end
 
+#creates view of all users
 MyApp.get "/users_view" do
   @users = User.all
   erb :"users/users_view"
 end
+
+#edits user
+MyApp.get "/edit_user/:id" do
+  @user = User.find_by_id(params[:id])
+  @user.name = params["name"]
+  @user.email = params["email"]
+  @user.password = params["password"]
+  @user.save
+  erb :"users/edit_success"
+end
+
+#deletes user
+MyApp.get "/delete_user/:id" do
+  @user = User.find_by_id(params[:id])
+  @user.delete
+  erb :"users/delete_success"
+end
+
